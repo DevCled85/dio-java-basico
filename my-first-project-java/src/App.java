e9af8d185e5bbd2bb9c923ffa4a7503cd53620a9
Cledson do Nascimento Pereira <142537029+DevCled85@users.noreply.github.com>
2023-10-12 02:30:27 -0400

    commit two

--- my-first-project-java/src/App.java
+++ my-first-project-java/src/App.java
@@ -1,5 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas a galera do Java...!");
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Nome";
+        String segundoNome = "Sobrenome";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
+
