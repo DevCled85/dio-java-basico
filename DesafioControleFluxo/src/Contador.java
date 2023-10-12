ca8712e50c149b1462c4e41758c3e3d0af9f2cc0
Cledson do Nascimento Pereira <142537029+DevCled85@users.noreply.github.com>
2023-10-12 19:59:28 -0400

    second project java basic

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        try (Scanner t1erminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt(); // Use nextInt() para ler um valor inteiro
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt(); // Use nextInt() para ler um valor inteiro
+
+            try {
+                contar(parametroUm, parametroDois);
+            } catch (ParametrosInvalidosException exception) { // Nome da exceção personalizada
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            }
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException(); // Lançar exceção se o primeiro for maior ou igual ao segundo
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println(parametroUm + i);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
+
